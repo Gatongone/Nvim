c8f553dbe7815bb06542af678d8743608fa2e3b8
Gatongone <gatongone@gmail.com>
2026-04-01 00:40:58 +0800

    fix: avoid lua file start with non-vim workspace.

--- lua/plug/langs/lua.lua
+++ lua/plug/langs/lua.lua
@@ -3,9 +3,12 @@ return
     lsp =
     {
         on_init = function(client)
-            local path = client.workspace_folders[1].name
-            if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-                return
+            if client.workspace_folders and #client.workspace_folders > 0 then
+                local path = client.workspace_folders[1].name
+                local uv = vim.uv or vim.loop
+                if uv.fs_stat(path .. '/.luarc.json') or uv.fs_stat(path .. '/.luarc.jsonc') then
+                    return
+                end
             end
 
             client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua,
